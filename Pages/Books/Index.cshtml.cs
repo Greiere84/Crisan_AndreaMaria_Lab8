44543d07b2470a0cc45348ee99fd29220d187d1a
ANDREA CRISAN <CRISAN.ANDREA3628@student.ubbcluj.ro>
2020-11-22 11:34:23 +0200

    Add project files.

---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Crisan_AndreaMaria_Lab8.Data;
+using Crisan_AndreaMaria_Lab8.Models;
+
+namespace Crisan_AndreaMaria_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Crisan_AndreaMaria_Lab8.Data.Crisan_AndreaMaria_Lab8Context _context;
+
+        public IndexModel(Crisan_AndreaMaria_Lab8.Data.Crisan_AndreaMaria_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b=>b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
